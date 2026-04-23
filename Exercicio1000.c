aaa6c2f6a5663ae0b2f554bc18d3eba3621979cd
Nikollas-Souza <nikollas.souza@unifesp.br>
2026-04-23 14:07:41 -0300

    Exercicio1000.c

---
+++ Exercicio1000.c
@@ -0,0 +1,17 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <Nikollas Beneveni de Souza>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 18/04/2026
+Objetivo    : Mostrar a mensagem "Hellow World!".
+Aprendizado : <O básico do básico de programação da linguagem C>
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+ 
+int main() {
+ 
+    printf("Hello World!\n");
+ 
+    return 0;
+}
